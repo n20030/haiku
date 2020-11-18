9bc3f5c3c9d2f0063acc7e476a5bf46a564cc770
bob <n20030@std.it-college.ac.jp>
2020-11-18 13:23:46 +0900

    first commit

---
+++ haiku.py
@@ -0,0 +1,25 @@
+import random
+import time
+
+def sl1():
+	time.sleep(1)
+def sl2():
+	time.sleep(2)
+
+#List of season words
+five_letters = ['oozorani', 'oiyukuka', 'akisameno', 'sayonarato', 'seminoyou', 'ninnnikune', 'hadukasii', 'tanosiiyo', 'kiminokoto', 'koikogare', 'natukesii']
+seven_characters = ['otirunamidani' ,'sakananoyouni', 'hanabitotomoni' , 'kurusimagireno' , 'hosihuruyoruno', 'tukimohohoemu' ]
+
+choice_five1 = random.choice(five_letters)
+choice_five2 = random.choice(five_letters)
+choice_seven = random.choice(seven_characters)
+
+
+#print(choice_five)
+#print(choice_seven)
+sl1()
+print(choice_five1)
+sl1()
+print(choice_seven)
+sl1()
+print(choice_five2)
