2082fcaf5f34a581f6b900d6dc23d6a814a01c11
bob <n20030@std.it-college.ac.jp>
2020-11-18 14:08:00 +0900

    文字を細かく

--- haiku.py
+++ haiku.py
@@ -8,8 +8,16 @@ def sl1():
 def sl2():
 	time.sleep(2)
 
-#List of season words
-five_letters = ['大空に''oozorani', 'oiyukuka', 'akisameno', 'sayonarato', 'seminoyou', 'ninnnikune', 'hadukasii', 'tanosiiyo', 'kiminokoto', 'koikogare', 'natukesii']
+
+subject_two_letters = ['夜', '海', '空', '昼', '石', '豆', '熊', '蝉']
+subject_three_letters = ['魚' , 'かもめ', '子猫', '雀'　, 'マグロ', 'つばめ']
+subject_four_letters = ['石橋'　,'なめくじ', 'うみうし' , 'かにかま', 'シマウマ', 'ウミガメ']
+
+
+predicate_one_letters = ['を', 'に', 'や', 'も', 'よ']
+predicate_two_letters = ['たち', 'の音'　, 'の目'　,]
+
+
 seven_characters = ['otirunamidani' ,'sakananoyouni', 'hanabitotomoni' , 'kurusimagireno' , 'hosihuruyoruno', 'tukimohohoemu' ]
 
 choice_five1 = random.choice(five_letters)
